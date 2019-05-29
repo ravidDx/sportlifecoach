9fb2d02e3927c97901dac0f9c1422ed74a1c5a30
TatyMaybeth <tattocsb_010994@hotmail.com>
2019-05-28 20:07:41 -0500

    cambio carpeta

--- Pagina Web/Fitapp/src/app/login/login.page.ts
+++ Pagina Web/Fitapp/src/app/login/login.page.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  
 
-  ngOnInit() {
+  constructor() {}
+
+   ngOnInit() {
   }
 
 }
