b6245360cf741cee8a2660121faa2b75808b5a46
ravidDx <raulbastidas@outlook.com>
2020-02-03 20:25:42 -0500

    correcciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBjyWK7eorJaa7oIEwKAeW6rQ3WzLaq1Vo",
+    authDomain: "miapp-158221.firebaseapp.com",
+    databaseURL: "https://miapp-158221.firebaseio.com",
+    projectId: "miapp-158221",
+    storageBucket: "gs://miapp-158221.appspot.com/",
+    messagingSenderId: "231674641543"
+  } 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyBjyWK7eorJaa7oIEwKAeW6rQ3WzLaq1Vo",
     authDomain: "miapp-158221.firebaseapp.com",
